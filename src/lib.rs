8a545b28f576045a349801a90b255c35abdcec00
Hendrik Sollich <hendrik@hoodie.de>
2019-12-25 19:12:56 +0100

    feat: add Yaml wrapper type

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,29 @@
+use std::{fs, path::Path};
+
 pub mod error;
 mod pathfinder;
 mod util;
 
 pub use pathfinder::PathFinder;
+
+/// Wrapper around `yaml_rust::Yaml` that implements `PathFinder`
+pub struct Yaml(yaml_rust::Yaml);
+
+impl std::ops::Deref for Yaml {
+    type Target = yaml_rust::Yaml;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Yaml {
+    /// Conveniently open a yaml file
+    pub fn open<P: AsRef<Path>>(path: &P) -> Result<Yaml, Box<dyn std::error::Error>> {
+        let file_content = fs::read_to_string(&path)?;
+        Self::parse(&file_content)
+    }
+
+    pub fn parse(file_content: &str) -> Result<Yaml, Box<dyn std::error::Error>> {
+        util::parse(file_content).map(Self)
+    }
+}
